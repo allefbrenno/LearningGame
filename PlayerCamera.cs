db49b9dafa8cb0e416faae59699d8fac6ef6f508
allef brenno <allefbrenno100@hotmail.com>
2018-08-20 21:39:36 -0300

    Código a ser alterado

---
+++ PlayerCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour {
+
+    public float smoothSpeed = 0.125f;
+    public Transform player;
+    public Vector3 offset;
+
+    private void FixedUpdate()
+    {
+
+        Vector3 desiredPosition = player.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed*Time.deltaTime);
+        transform.position = smoothedPosition;
+        
+    }
+}
+
+
